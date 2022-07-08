a0810c6e5cbcd400f5bbe9bebb8474f22a0c8d90
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-07-08 13:56:51 -0300

    nice json

--- Theory.js
+++ Theory.js
@@ -12,6 +12,7 @@ var authors = "Annontations6";
 var version = "1.0.0";
 
 var currency;
+let letter = "a";
 
 var init = () => {
     currency = theory.createCurrency();
@@ -259,5 +260,161 @@ var getPrimaryEquation = () => {
 
     //result += "a_{69}";
 
+    // Break:
+    //null
+
     return result;
 }
+
+var getSecondaryEquation = () => { 
+    result = theory.latexSymbol + "=\\max\\rho"
+
+    result += "^{0.6}"
+
+    /*
+    1
+    2
+    3
+    4
+    5
+    6
+    7
+    8
+    9
+    10
+    11
+    12
+    13
+    14
+    15
+    16
+    17
+    18
+    19
+    20
+    21
+    22
+    23
+    24
+    25
+    26
+    27
+    28
+    29
+    30
+    31
+    32
+    33
+    34
+    35
+    36
+    37
+    38
+    39
+    40
+    41
+    42
+    43
+    44
+    45
+    46
+    47
+    48
+    49
+    50
+    51
+    52
+    53
+    54
+    56
+    57
+    58
+    59
+    60
+    61
+    62
+    63
+    64
+    65
+    66
+    67
+    68
+    69
+    70
+    71
+    72
+    73
+    74
+    75
+    76
+    77
+    78
+    79
+    80
+    81
+    82
+    83
+    84
+    85
+    86
+    87
+    88
+    89
+    90
+    91
+    92
+    93
+    94
+    95
+    96
+    97
+    98
+    99
+    100
+    Aleardy!
+    */
+
+    return result
+};
+
+
+var getPublicationMultiplier = (tau) => tau.pow(0.174) / BigNumber.THREE;
+var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.174}}{3}";
+var getTau = () => currency.value.pow(0.6);
+var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
+
+var getA1 = (level) => BigNumber.ZERO;
+var getA2 = (level) => BigNumber.ZERO;
+var getA3 = (level) => BigNumber.ZERO;
+var getA4 = (level) => BigNumber.ZERO;
+var getA5 = (level) => BigNumber.ZERO;
+var getA6 = (level) => BigNumber.ZERO;
+var getA7 = (level) => BigNumber.ZERO;
+var getA8 = (level) => BigNumber.ZERO;
+var getA9 = (level) => BigNumber.ZERO;
+
+init();
+
+/* INIT TWO */
+
+var init2 = () => {
+    //1
+    //2
+    //3
+
+    switch ("a") {
+        case "a":
+            letter = "a";
+            break;
+        case "b":
+            letter = "b";
+            break;
+    }
+
+    //idk
+}
+
+init2();
+
+//somewhere
+//XD
