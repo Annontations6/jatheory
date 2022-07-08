6e5f7a878dcfbb453c636272700ee61792491e3f
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-07-08 13:13:49 -0300

    Create Theory.js

---
+++ Theory.js
@@ -0,0 +1,12 @@
+import { ExponentialCost, FreeCost, LinearCost } from "./api/Costs";
+import { Localization } from "./api/Localization";
+import { BigNumber } from "./api/BigNumber";
+import { theory } from "./api/Theory";
+import { Utils } from "./api/Utils";
+
+
+var id = "99209";
+var name = "Jatheory";
+var description = "when so get!";
+var authors = "Annontations6";
+var version = "1.0.0";
