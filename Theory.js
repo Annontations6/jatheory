a6babf03c9826f286e7e9538fddd497e51d0451a
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-07-08 13:42:26 -0300

    nice

--- Theory.js
+++ Theory.js
@@ -90,4 +90,174 @@ var init = () => {
         a9.getDescription = (_) => Utils.getMath(getDesc(a9.level));
         a9.getInfo = (amount) => Utils.getMathTo(getDesc(a9.level), getDesc(a9.level + amount));
     }
+
+    /////////////////////
+    // Permanent Upgrades
+    theory.createPublicationUpgrade(0, currency, 1e10);
+    theory.createBuyAllUpgrade(1, currency, 1e13);
+    theory.createAutoBuyerUpgrade(2, currency, 1e30);
+
+    ///////////////////////
+    //// Milestone Upgrades
+    theory.setMilestoneCost(new LinearCost(25, 25));
+
+    /////////////////
+    //// Achievements
+
+    ///////////////////
+    //// Story chapters
+}
+
+var tick = (elapsedTime, multiplier) => {
+    currency.value += a1.level * 2
+    currency.value += a2.level * 30
+    currency.value += a3.level * 40
+    currency.value += a4.level * 200
+    currency.value += a5.level * 7000
+    currency.value += a6.level * 1.1e5
+    currency.value += a7.level * 9e5
+    currency.value += a8.level * 4e6
+    currency.value += a9.level * 3e7
+}
+
+var getPrimaryEquation = () => {
+    let result = "\\dot{\\rho} = a_1";
+
+    result += "a_2";
+
+    result += "a_3";
+
+    result += "a_4";
+
+    result += "a_5";
+
+    result += "a_6";
+
+    result += "a_7";
+
+    result += "a_8";
+
+    result += "a_9";
+
+    //result += "a_{10}";
+
+    //result += "a_{11}";
+
+    //result += "a_{12}";
+
+    //result += "a_{13}";
+
+    //result += "a_{14}";
+
+    //result += "a_{15}";
+
+    //result += "a_{16}";
+
+    //result += "a_{17}";
+
+    //result += "a_{18}";
+
+    //result += "a_{19}";
+
+    //result += "a_{20}";
+
+    //result += "a_{21}";
+
+    //result += "a_{22}";
+
+    //result += "a_{23}";
+
+    //result += "a_{24}";
+
+    //result += "a_{25}";
+
+    //result += "a_{26}";
+
+    //result += "a_{27}";
+
+    //result += "a_{28}";
+
+    //result += "a_{29}";
+
+    //result += "a_{30}";
+
+    //result += "a_{31}";
+
+    //result += "a_{32}";
+
+    //result += "a_{33}";
+
+    //result += "a_{34}";
+
+    //result += "a_{35}";
+
+    //result += "a_{36}";
+
+    //result += "a_{37}";
+
+    //result += "a_{38}";
+
+    //result += "a_{39}";
+
+    //result += "a_{40}";
+
+    //result += "a_{41}";
+
+    //result += "a_{42}";
+
+    //result += "a_{43}";
+
+    //result += "a_{44}";
+
+    //result += "a_{45}";
+
+    //result += "a_{46}";
+
+    //result += "a_{47}";
+
+    //result += "a_{48}";
+
+    //result += "a_{49}";
+
+    //result += "a_{50}";
+
+    //result += "a_{51}";
+
+    //result += "a_{52}";
+
+    //result += "a_{53}";
+
+    //result += "a_{54}";
+
+    //result += "a_{55}";
+
+    //result += "a_{56}";
+
+    //result += "a_{57}";
+
+    //result += "a_{58}";
+
+    //result += "a_{59}";
+
+    //result += "a_{60}";
+
+    //result += "a_{61}";
+
+    //result += "a_{62}";
+
+    //result += "a_{63}";
+
+    //result += "a_{64}";
+
+    //result += "a_{65}";
+
+    //result += "a_{66}";
+
+    //result += "a_{67}";
+
+    //result += "a_{68}";
+
+    //result += "a_{69}";
+
+    return result;
 }
